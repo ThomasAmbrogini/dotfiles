9d130b37747140cb754da90a23148320086cbef7
Thomas Ambrogini <thomas.ambrogini@gmail.com>
2024-06-19 08:58:17 +0200

    chore: add live grep search with telescope
    *   using `<leader>lg` it is possible to live grep for words inside the
        current directory. It will also search inside the files present in
        directories and subdirectories

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -4,9 +4,10 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
         -- See `:help telescope.builtin`
-         
+
         local builtin = require("telescope.builtin")
         vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+        vim.keymap.set('n', '<leader>lg', builtin.live_grep, { desc = '[L]ive [G]rep' })
 
     end,
 }
