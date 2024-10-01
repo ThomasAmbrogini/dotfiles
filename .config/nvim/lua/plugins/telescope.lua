013129bffbe8c910ff49d23189a0b0b6390640ea
Thomas Ambrogini <thomas.ambrogini@gmail.com>
2024-10-01 12:40:47 +0200

    chore: change telescope layout view

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -9,6 +9,18 @@ return {
         vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
         vim.keymap.set('n', '<leader>lg', builtin.live_grep, { desc = '[L]ive [G]rep' })
 
+        require("telescope").setup{
+            defaults = {
+                layout_strategy = "vertical",
+                layout_config = {
+                    vertical = {
+                        width = 0.99,
+                        height = 0.99,
+                    },
+                },
+            },
+        }
+
     end,
 }
 
